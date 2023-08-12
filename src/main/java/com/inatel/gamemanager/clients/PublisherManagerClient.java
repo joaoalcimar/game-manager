6d2b1f9636f3ef13216d60ff19526eb3793be09b
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-08-12 13:06:58 -0300

    Create publisher manager client skeleton

---
+++ src/main/java/com/inatel/gamemanager/clients/PublisherManagerClient.java
@@ -0,0 +1,11 @@
+package com.inatel.gamemanager.clients;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PublisherManagerClient {
+
+    public boolean validatePublisher(String publisherId) {
+        return true;
+    }
+}
