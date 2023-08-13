a6bfc02b7ba1c009a9210202d4387d1ac53720c3
joaoalcimar <joao.alcimar.junior@gmail.com>
2023-08-13 09:56:26 -0300

    Create suite of unit tests

---
+++ src/test/java/com/inatel/gamemanager/GameManagerApplicationSuiteTests.java
@@ -0,0 +1,59 @@
+package com.inatel.gamemanager;
+
+import com.inatel.gamemanager.clients.publishermanager.PublisherManagerClientTest;
+import com.inatel.gamemanager.clients.publishermanager.models.RegisterRequestBodyTest;
+import com.inatel.gamemanager.controllers.GameManagementControllerTest;
+import com.inatel.gamemanager.controllers.PublisherManagerListenerControllerTest;
+import com.inatel.gamemanager.exceptions.settings.ExceptionDetailsTest;
+import com.inatel.gamemanager.exceptions.settings.GlobalExceptionHandlerTest;
+import com.inatel.gamemanager.models.dtos.requests.GameRequestTest;
+import com.inatel.gamemanager.models.dtos.responses.GameResponseTest;
+import com.inatel.gamemanager.models.dtos.responses.HttpResponseTest;
+import com.inatel.gamemanager.models.entities.GameTest;
+import com.inatel.gamemanager.services.GameManagementServiceTest;
+import com.inatel.gamemanager.utils.JsonConverterUtilTest;
+import com.inatel.gamemanager.utils.RestTemplateUtilTest;
+import com.inatel.gamemanager.utils.TimeFormatterUtilTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+		// clients dir
+		RegisterRequestBodyTest.class,
+		PublisherManagerClientTest.class,
+
+		// controllers dir
+		GameManagementControllerTest.class,
+		PublisherManagerListenerControllerTest.class,
+
+		// exceptions dir
+		ExceptionDetailsTest.class,
+		GlobalExceptionHandlerTest.class,
+
+		// models dir
+		GameRequestTest.class,
+		GameResponseTest.class,
+		HttpResponseTest.class,
+		GameTest.class,
+
+		// services dir
+		GameManagementServiceTest.class,
+
+		// utils dir
+		JsonConverterUtilTest.class,
+		RestTemplateUtilTest.class,
+		TimeFormatterUtilTest.class
+})
+
+public class GameManagerApplicationSuiteTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+}
+
